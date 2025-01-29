1b88d48e9f617930b32421dda3d048a7a0a91fd8
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-28 23:51:38 -0300

    agrego experiencia

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <main style={{ padding: '20px', maxWidth: '800px', margin: 'auto' }}>
-      <h1>Pedro Eduardo Donoso Ramírez</h1>
-      <p>Desarrollador Full Stack | +56 9 5396 8887 | <a href="mailto:pedro.eduardo.donoso@gmail.com">pedro.eduardo.donoso@gmail.com</a></p>
+    <main className='p-6 max-w-2xl mx-auto bg-white rounded-lg shadow-md'>
+      <h1 className='text-2xl font-bold mb-2 text-center'>Pedro Eduardo Donoso Ramírez</h1>
+      <p className='mt-2'>Desarrollador Full Stack | +56 9 5396 8887 | <a href="mailto:pedro.eduardo.donoso@gmail.com" className='text-blue-800'>pedro.eduardo.donoso@gmail.com</a></p>
 
-      <section>
-        <h2>Perfil Profesional</h2>
+      <section className='mt-5'>
+        <h2 className='text-2xl font-bold mb-2'>Perfil Profesional</h2>
         <p>
           Desarrollador Full Stack con amplia experiencia en la creación y mantenimiento de aplicaciones web
           y móviles. Especializado en la optimización de sitios web y tiendas en línea, con sólidas habilidades
@@ -17,31 +17,31 @@ const Home: React.FC = () => {
         </p>
       </section>
 
-      <section>
-        <h2>Experiencia Laboral</h2>
-        <h3>Desarrollador en Agencia Amigo Pyme (2022-2024)</h3>
-        <ul>
-          <li>Desarrollo y mantenimiento de aplicaciones utilizando Kotlin Compose para asegurar un rendimiento óptimo y una experiencia de usuario fluida.</li>
-          <li>Implementación de soluciones innovadoras en Angular 19 para aplicaciones móviles multiplataforma de alta calidad.</li>
-          <li>Administración y optimización de tiendas en línea con WooCommerce, mejorando la funcionalidad y la experiencia de compra.</li>
-          <li>Integración de soluciones de pago seguras utilizando Flow y Transbank, garantizando transacciones confiables.</li>
+      <section className='mt-5'>
+        <h2 className='text-2xl font-bold mb-2'>Experiencia Laboral</h2>
+        <h3 className='mb-2 underline'>Desarrollador en <i>Agencia Amigo Pyme</i> (2022-2024)</h3>
+        <ul className='list-disc list-inside ml-5 mb-5'>
+          <li>Desarrollo y mantenimiento de aplicaciones utilizando <strong>Kotlin Compose</strong> para asegurar un rendimiento óptimo y una experiencia de usuario fluida.</li>
+          <li>Implementación de soluciones innovadoras en <strong>Angular 19</strong> para aplicaciones móviles multiplataforma de alta calidad.</li>
+          <li>Administración y optimización de tiendas en línea con <strong>WooCommerce</strong>, mejorando la funcionalidad y la experiencia de compra.</li>
+          <li>Integración de soluciones de pago seguras utilizando <strong>Flow</strong> y <strong>Transbank</strong>, garantizando transacciones confiables.</li>
         </ul>
 
-        <h3>Desarrollador en Desafío Latam (2019 - 2021)</h3>
-        <ul>
-          <li>Creación y mantenimiento de aplicaciones en Java y PHP, asegurando la funcionalidad y escalabilidad de los sistemas.</li>
-          <li>Desarrollo de aplicaciones móviles de alto rendimiento en Angular y React Native.</li>
-          <li>Aplicación de metodologías ágiles con herramientas como GoodDay y BaseCamp para una planificación y ejecución eficiente de proyectos.</li>
-          <li>Realización de pruebas exhaustivas y gestión de merges en GitLab para garantizar la compatibilidad y el rendimiento óptimo.</li>
-          <li>Diseño de interfaces de usuario en Adobe XD, mejorando la experiencia del usuario.</li>
+        <h3 className='mb-2 underline'>Desarrollador en <i>Desafío Latam</i> (2019 - 2021)</h3>
+        <ul className='list-disc list-inside ml-5 mb-5'>
+          <li>Creación y mantenimiento de aplicaciones en <strong>Java</strong> y <strong>PHP</strong>, asegurando la funcionalidad y escalabilidad de los sistemas.</li>
+          <li>Desarrollo de aplicaciones móviles de alto rendimiento en <strong>Angular </strong> y <strong>React Native</strong>.</li>
+          <li>Aplicación de <strong>Metodologías Ágiles</strong> con herramientas como GoodDay y BaseCamp para una planificación y ejecución eficiente de proyectos.</li>
+          <li>Realización de pruebas exhaustivas y gestión de merges en <strong>GitLab</strong> para garantizar la compatibilidad y el rendimiento óptimo.</li>
+          <li>Diseño de interfaces de usuario en <strong>Adobe XD</strong>, mejorando la experiencia del usuario.</li>
         </ul>
 
-        <h3>Desarrollador en CCS (2017 - 2019)</h3>
-        <ul>
-          <li>Creación de aplicaciones móviles en Angular y React Native, enfocándome en la optimización del rendimiento y la experiencia del usuario.</li>
-          <li>Colaboración en el diseño de interfaces de usuario atractivas y funcionales, utilizando componentes reutilizables.</li>
-          <li>Implementación de APIs y servicios externos para enriquecer la funcionalidad de las aplicaciones.</li>
-          <li>Participación activa en reuniones de planificación y revisión de proyectos bajo metodologías ágiles (Scrum).</li>
+        <h3 className='mb-2 underline'>Desarrollador en <i>CCS</i> (2017 - 2019)</h3>
+        <ul className='list-disc list-inside ml-5 mb-5'>
+          <li>Creación de aplicaciones móviles en <strong>Angular</strong> y <strong>React Native</strong>, enfocándome en la optimización del rendimiento y la experiencia del usuario.</li>
+          <li>Colaboración en el diseño de interfaces de usuario atractivas y funcionales, utilizando <strong>componentes</strong> reutilizables.</li>
+          <li>Implementación de <strong>APIs</strong> y servicios externos para enriquecer la funcionalidad de las aplicaciones.</li>
+          <li>Participación activa en reuniones de <strong>planificación</strong> y revisión de proyectos bajo metodologías ágiles (Scrum).</li>
         </ul>
       </section>
 
