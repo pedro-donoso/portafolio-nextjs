a880307d5240323e1a4f7d8c379525ce432924a7
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-29 00:38:54 -0300

    agrego bordes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
 		<div className='bg-cyan-200 min-h-screen'>
 			<Topbar />
-			<main className='p-8 max-4 max-w-5xl mx-auto'>
+			<main className='pb-10 max-4 max-w-5xl mx-auto'>
 				{children}
 			</main>
 		</div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <main className='p-6 max-w-4xl mx-auto bg-white rounded-lg shadow-md'>
+    <main className='px-16 py-10 max-w-4xl mx-auto bg-white rounded-lg shadow-md'>
       <h1 className='text-2xl font-bold mb-2 text-center'>Pedro Eduardo Donoso Ramírez</h1>
       <p className='mt-2 text-center'>Desarrollador Full Stack | +56 9 5396 8887 | <a href="mailto:pedro.eduardo.donoso@gmail.com" className='text-blue-800'>pedro.eduardo.donoso@gmail.com</a></p>
 
