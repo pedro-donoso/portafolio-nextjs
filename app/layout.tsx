32a7dd66f8a60008b7c72c2670d409678d9aabf0
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-30 03:40:32 -0300

    fuente Roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
-import Topbar from '@/components/Topbar';
-import './globals.css';
+import Topbar from "@/components/Topbar";
+import "./globals.css";
+import Head from "next/head";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-   <html lang="es">
-    <body>
-     <div className="bg-cyan-200 min-h-screen">
-      <Topbar />
-      <main className="pb-10 max-4 max-w-5xl mx-auto">{children}</main>
-     </div>
-    </body>
-   </html>
-  );
+export default function RootLayout({
+ children,
+}: {
+ children: React.ReactNode;
+}) {
+ return (
+  <html lang="es">
+   <body className="font-roboto">
+    <div className="bg-cyan-200 min-h-screen">
+     <Topbar />
+     <main className="pb-10 max-4 max-w-5xl mx-auto">{children}</main>
+    </div>
+   </body>
+  </html>
+ );
 }
