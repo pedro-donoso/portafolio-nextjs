b70af8cc8e8e8c8b451b503e8dcc3b498e0679d9
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-24 01:52:58 -0300

    agrego iconos

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,195 +2,219 @@ import React from 'react';
 
 const Skills: React.FC = () => {
 	return (
-  <section className="mt-5" id="habilidades">
-   <h2 className="text-xl font-bold mb-2">Habilidades</h2>
-   <h3>Lenguajes de Programación:</h3>
-   <p className="ml-5 mb-5 text-justify mt-2">
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Kotlin-Dark.svg"
-     alt="Kotlin"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Kotlin"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Java-Dark.svg"
-     alt="Java"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Java"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/PHP-Dark.svg"
-     alt="PHP"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="PHP"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/JavaScript.svg"
-     alt="JavaScript"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="JavaScript"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Ruby.svg"
-     alt="Ruby"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Ruby"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Matlab-Dark.svg"
-     alt="Matlab"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Matlab"
-    />
-   </p>
+    <section className="mt-5" id="habilidades">
+      <h2 className="mb-2 text-xl font-bold">Habilidades</h2>
+      <h3>Lenguajes de Programación:</h3>
+      <p className="mt-2 mb-5 ml-5 text-justify">
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Kotlin-Dark.svg"
+          alt="Kotlin"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Kotlin"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Java-Dark.svg"
+          alt="Java"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Java"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/PHP-Dark.svg"
+          alt="PHP"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="PHP"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/JavaScript.svg"
+          alt="JavaScript"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="JavaScript"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Ruby.svg"
+          alt="Ruby"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Ruby"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Matlab-Dark.svg"
+          alt="Matlab"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Matlab"
+        />
+      </p>
 
-   <h3>Desarrollo Frontend:</h3>
-   <p className="ml-5 mb-5 text-justify mt-2">
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/HTML.svg"
-     alt="HTML"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="HTML"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/CSS.svg"
-     alt="CSS"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="CSS"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Bootstrap.svg"
-     alt="Bootstrap"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Bootstrap"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/TailwindCSS-Dark.svg"
-     alt="Tailwind"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Tailwind"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Angular-Dark.svg"
-     alt="Angular"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Angular"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/React-Dark.svg"
-     alt="React"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="React"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/VueJS-Dark.svg"
-     alt="Vue"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Vue"
-    />
-   </p>
+      <h3>Desarrollo Frontend:</h3>
+      <p className="mt-2 mb-5 ml-5 text-justify">
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/HTML.svg"
+          alt="HTML"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="HTML"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/CSS.svg"
+          alt="CSS"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="CSS"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Bootstrap.svg"
+          alt="Bootstrap"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Bootstrap"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/TailwindCSS-Dark.svg"
+          alt="Tailwind"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Tailwind"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Angular-Dark.svg"
+          alt="Angular"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Angular"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/React-Dark.svg"
+          alt="React"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="React"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/VueJS-Dark.svg"
+          alt="Vue"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Vue"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/NextJS-Dark.svg"
+          alt="NextJS"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="NextJS"
+        />
+      </p>
 
-   <h3>Desarrollo Backend:</h3>
-   <p className="ml-5 mb-5 text-justify mt-2">
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Rails.svg"
-     alt="Rails"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Rails"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/NodeJS-Dark.svg"
-     alt="Nodejs"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Nodejs"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/ExpressJS-Dark.svg"
-     alt="Express"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Express"
-    />
-   </p>
+      <h3>Desarrollo Backend:</h3>
+      <p className="mt-2 mb-5 ml-5 text-justify">
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Rails.svg"
+          alt="Rails"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Rails"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/NodeJS-Dark.svg"
+          alt="Nodejs"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Nodejs"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/ExpressJS-Dark.svg"
+          alt="Express"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Express"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/NestJS-Dark.svg"
+          alt="NestJS"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="NestJS"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Prisma.svg"
+          alt="Prisma"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Prisma"
+        />
+      </p>
 
-   <h3>Bases de Datos:</h3>
-   <p className="ml-5 mb-5 text-justify mt-2">
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/PostgreSQL-Dark.svg"
-     alt="PostgreSQL"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="PostgreSQL"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/SQLite.svg"
-     alt="SQLite"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="SQLite"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/MongoDB.svg"
-     alt="MongoDB"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="MongoDB"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/MySQL-Dark.svg"
-     alt="MySQL"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="MySQL"
-    />
-   </p>
+      <h3>Bases de Datos:</h3>
+      <p className="mt-2 mb-5 ml-5 text-justify">
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/PostgreSQL-Dark.svg"
+          alt="PostgreSQL"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="PostgreSQL"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/SQLite.svg"
+          alt="SQLite"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="SQLite"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/MongoDB.svg"
+          alt="MongoDB"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="MongoDB"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/MySQL-Dark.svg"
+          alt="MySQL"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="MySQL"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Supabase-Dark.svg"
+          alt="Supabase"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Supabase"
+        />
+      </p>
 
-   <h3>Platform as a Service (PaaS):</h3>
-   <p className="ml-5 mb-5 text-justify mt-2">
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Netlify-Dark.svg"
-     alt="Netlify"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Netlify"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Vercel-Dark.svg"
-     alt="Vercel"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Vercel"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Heroku.svg"
-     alt="Heroku"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Heroku"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Firebase-Dark.svg"
-     alt="Firebase"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Firebase"
-    />
-   </p>
+      <h3>Platform as a Service (PaaS):</h3>
+      <p className="mt-2 mb-5 ml-5 text-justify">
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Netlify-Dark.svg"
+          alt="Netlify"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Netlify"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Vercel-Dark.svg"
+          alt="Vercel"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Vercel"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Heroku.svg"
+          alt="Heroku"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Heroku"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Firebase-Dark.svg"
+          alt="Firebase"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Firebase"
+        />
+      </p>
 
-   <h3>Platform as a Service (PaaS):</h3>
-   <p className="ml-5 mb-5 text-justify mt-2">
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Git.svg"
-     alt="Git"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="Git"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Github-Dark.svg"
-     alt="GitHub"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="GitHub"
-    />
-    <img
-     src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/GitLab-Dark.svg"
-     alt="GitLab"
-     className="inline w-8 h-8 mr-2 transition-transform duration-200 transform hover:scale-125"
-     title="GitLab"
-    />
-   </p>
-  </section>
- );
+      <h3>Sistema de Control de Versiones:</h3>
+      <p className="mt-2 mb-5 ml-5 text-justify">
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Git.svg"
+          alt="Git"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="Git"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Github-Dark.svg"
+          alt="GitHub"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="GitHub"
+        />
+        <img
+          src="https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/GitLab-Dark.svg"
+          alt="GitLab"
+          className="inline mr-2 w-8 h-8 transition-transform duration-200 transform hover:scale-125"
+          title="GitLab"
+        />
+      </p>
+    </section>
+  );
 };
 
 export default Skills;
